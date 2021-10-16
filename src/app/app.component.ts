2b35ee628088789c71ef4f821bc9e859dd6a35d6
tchfer <tchfernando@gmail.com>
2021-10-16 00:27:37 -0300

    using ngFor directive to loop through an array of objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  public photos = [
+    {
+      uri: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      description: 'landscape'
+    },
+    {
+      uri: "https://images.unsplash.com/photo-1511884642898-4c92249e20b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      description: 'landscape'
+    }
+  ]
   }
