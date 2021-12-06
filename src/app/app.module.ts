0073defe5b0821a08b8aff78e37276b170e416cf
tchfer <tchfernando@gmail.com>
2021-12-06 01:37:17 -0300

    add routing module to open different components according to path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app.routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     PhotosModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from "@angular/core";
 
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 
 @NgModule({
   declarations: [
     PhotoComponent,
-    PhotoListComponent
+    PhotoListComponent,
+    PhotoFormComponent
   ],
   imports: [
     HttpClientModule,
