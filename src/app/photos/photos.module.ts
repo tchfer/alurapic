8a1308cd08d7d537d14929457eacac40d81dc657
tchfer <tchfernando@gmail.com>
2021-12-06 01:10:07 -0300

    import common Module to ake directives work

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,14 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from "@angular/core";
 
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
 
+
 @NgModule({
   declarations: [
     PhotoComponent,
     PhotoListComponent
   ],
-  imports: [ HttpClientModule ]
+  imports: [
+    HttpClientModule,
+    CommonModule
+  ]
 })
 export class PhotosModule {}
