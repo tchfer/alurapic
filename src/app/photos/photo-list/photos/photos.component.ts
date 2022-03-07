01022065b9ef08337380f8e70be434dd964765fa
tchfer <tchfernando@gmail.com>
2022-03-07 01:36:21 -0300

    add group column function to display photos

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,17 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
   selector: 'ap-photos',
   templateUrl: './photos.component.html',
-  styleUrls: ['./photos.component.scss']
+  styleUrls: ['./photos.component.scss'],
 })
-export class PhotosComponent implements OnInit {
-
+export class PhotosComponent implements OnChanges {
   @Input() photos: Photo[] = [];
-  constructor() { }
+  public rows: any[] = [];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.rows = this.groupColumns(this.photos);
   }
 
+  public groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for(let index = 0; index < photos.length; index+=3) {
+      newRows.push(photos.slice(index, index + 3))
+    }
+    return newRows
+  }
 }
