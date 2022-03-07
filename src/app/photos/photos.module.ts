30bb2435799ca9024426d2c3f6ddc05f1689c42e
tchfer <tchfernando@gmail.com>
2022-03-07 01:17:03 -0300

    fix selectors and add photos component

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from "@angular/core";
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 
 @NgModule({
   declarations: [
     PhotoComponent,
     PhotoListComponent,
-    PhotoFormComponent
+    PhotoFormComponent,
+    PhotosComponent
   ],
   imports: [
     HttpClientModule,
