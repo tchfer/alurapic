80edbba736f1808c4f233014b2f275e11afec672
tchfer <tchfernando@gmail.com>
2022-03-08 09:40:05 -0300

    organize project with sub modules

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,23 @@
+import { PhotoModule } from '../photo/photo.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+
+import { FilterByDescription } from './filter-by-description.pipe';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotosComponent } from './photos/photos.component';
+
+
+@NgModule({
+  declarations: [
+    PhotoListComponent,
+    PhotosComponent,
+    LoadButtonComponent,
+    FilterByDescription
+  ],
+  imports: [
+    CommonModule,
+    PhotoModule
+  ]
+})
+export class PhotoListModule {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,27 +1,14 @@
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from "@angular/core";
 
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
-
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescription,
-    LoadButtonComponent
-  ],
   imports: [
-    HttpClientModule,
-    CommonModule
+    PhotoModule,
+    PhotoFormModule,
+    PhotoListModule,
   ]
 })
 export class PhotosModule {}
