ec38c1cd9c6de07e6e37703aee1c17fb979c42ec
tchfer <tchfernando@gmail.com>
2022-03-07 21:37:33 -0300

    add resolver to load data before rendering content

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -13,16 +13,10 @@ export class PhotoListComponent implements OnInit {
   public photos: Photo[] = [];
   public filter: string = '';
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-    ) {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   public ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService.listFromUser(userName).subscribe((photos) => {
-      this.photos = photos;
-    });
+    this.photos = this.activatedRoute.snapshot.data['photos'];
   }
 
 }
