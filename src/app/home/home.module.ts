8c63a5889d6e3cf6f79115e2ac4f010cb3d2da2b
tchfer <tchfernando@gmail.com>
2022-03-15 02:05:16 -0300

    add signin component

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,7 @@
+import { SignInComponent } from './signin/signin.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [ SignInComponent ]
+})
+export class HomeModule  {}
