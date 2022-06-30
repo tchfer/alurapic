acbd0552941d233c41f75de02f5e1273a6a925b1
tchfer <tchfernando@gmail.com>
2022-06-30 00:34:30 -0300

    create shared error message for invalid form

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { SignInComponent } from './signin/signin.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
   declarations: [ SignInComponent ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VMessageModule
   ],
 })
 export class HomeModule  {}
---
+++ src/app/shared/components/vmessage/vmessage.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { VMessageComponent } from './vmessage.component';
+
+@NgModule({
+  declarations: [VMessageComponent],
+  imports: [CommonModule],
+  exports: [VMessageComponent],
+})
+export class VMessageModule {}
