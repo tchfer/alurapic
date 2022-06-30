d6dff4cdd247c9d0be7f48244f380f29ce532e65
tchfer <tchfernando@gmail.com>
2022-06-30 00:54:00 -0300

    auth service and login implemented

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
-  templateUrl: './signin.component.html'
+  templateUrl: './signin.component.html',
 })
 export class SignInComponent implements OnInit {
-
   loginForm: FormGroup = new FormGroup({});
 
   constructor(
-    private readonly formBuilder: FormBuilder
-  ) { }
+    private readonly formBuilder: FormBuilder,
+    private readonly authService: AuthService
+  ) {}
 
   public ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
-    })
+      password: ['', Validators.required],
+    });
+  }
+
+  public login(): void {
+    const { username, password } = this.loginForm.value;
+    this.authService.authenticate(username, password).subscribe(
+      () => console.log('Login realizado com sucesso'),
+      (err) => {
+        console.log(err);
+        this.loginForm.reset();
+      }
+    );
   }
 }
