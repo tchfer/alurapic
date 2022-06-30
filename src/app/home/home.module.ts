d93cd573aada4a5ee9d774a7194180d9f13762c6
tchfer <tchfernando@gmail.com>
2022-06-30 00:19:47 -0300

    add formControl and validators

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,14 @@
-import { SignInComponent } from './signin/signin.component';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
-  declarations: [ SignInComponent ]
+  declarations: [ SignInComponent ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
 })
 export class HomeModule  {}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   templateUrl: './signin.component.html'
 })
-export class SignInComponent {}
+export class SignInComponent implements OnInit {
+
+  loginForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private readonly formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+}
